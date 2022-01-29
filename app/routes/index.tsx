f56e6f1df58699d833332a9ff38bcf1ba7a26b89
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-01-28 22:14:42 -0600

    👑

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,7 @@
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Qwik</h1>
+      <h1>Welcome to Qwik: {Date.now()}</h1>
       <ul>
         <li>
           <a
