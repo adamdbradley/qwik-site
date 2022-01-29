b84473fe8059ae2a5ba632e24acc0e621a071694
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-01-28 23:12:53 -0600

    🍧

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,6 @@
 import { Links, LiveReload, Meta, Outlet } from "remix";
 import type { MetaFunction } from "remix";
+import { getQwikLoaderScript } from "@builder.io/qwik/server";
 
 export const meta: MetaFunction = () => {
   return { title: "Qwik" };
@@ -13,10 +14,18 @@ export default function App() {
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
+        <script
+          dangerouslySetInnerHTML={{
+            __html:
+              getQwikLoaderScript() +
+              `console.log("⚡️ QwikLoader Ready: ${Date.now()} ⚡️")`,
+          }}
+        />
       </head>
       <body>
         <Outlet />
         {process.env.NODE_ENV === "development" && <LiveReload />}
+        <script />
       </body>
     </html>
   );
