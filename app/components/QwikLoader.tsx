5ee2c8937da8b32e40d90827c389b81438a26d88
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-10-13 11:02:55 -0500

    🐼

--- app/components/QwikLoader.tsx
+++
@@ -1,9 +0,0 @@
-import { QWIK_LOADER } from "../loader";
-
-export const QwikLoader = () => (
-  <script
-    dangerouslySetInnerHTML={{
-      __html: QWIK_LOADER,
-    }}
-  />
-);
--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,12 +1,3 @@
-import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
-
-// @ts-ignore
-import * as build from "../build";
-
-const handleRequest = createPagesFunctionHandler({
-  build
-});
-
-export function onRequest(context) {
-  return handleRequest(context);
+export async function onRequest() {
+  return new Response("functions response");
 }
