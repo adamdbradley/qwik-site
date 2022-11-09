b94df7174e932b42d6400befdcefa09d939a6993
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-11-09 14:38:51 -0600

    👻

--- adaptors/cloudflare-pages/vite.config.ts
+++ adaptors/cloudflare-pages/vite.config.ts
@@ -17,30 +17,30 @@ export default extendConfig(baseConfig, () => {
       cloudflarePagesAdaptor({
         staticGenerate: true,
       }),
-      // {
-      //   name: "cf-hack",
-      //   enforce: "post",
-      //   configResolved(config) {
-      //     root = config.root;
-      //     console.log("root", root);
-      //   },
-      //   async closeBundle() {
-      //     return new Promise((resolve) => {
-      //       setTimeout(() => {
-      //         const routesPath = path.join(root, "dist", "_routes.json");
-      //         console.log("closeBundle1", routesPath);
-      //         const routes = JSON.parse(fs.readFileSync(routesPath, "utf8"));
-      //         routes.include = ["/*"];
-      //         routes.exclude = routes.exclude.filter((r) => {
-      //           return r.startsWith("/");
-      //         });
-      //         fs.writeFileSync(routesPath, JSON.stringify(routes, null, 2));
-      //         console.log("closeBundle routes", routes);
-      //         resolve();
-      //       }, 1000);
-      //     });
-      //   },
-      // },
+      {
+        name: "cf-hack",
+        enforce: "post",
+        configResolved(config) {
+          root = config.root;
+          console.log("root", root);
+        },
+        async closeBundle() {
+          return new Promise((resolve) => {
+            setTimeout(() => {
+              const routesPath = path.join(root, "dist", "_routes.json");
+              console.log("closeBundle1", routesPath);
+              const routes = JSON.parse(fs.readFileSync(routesPath, "utf8"));
+              routes.include = ["/*"];
+              routes.exclude = routes.exclude.filter((r) => {
+                return r.startsWith("/");
+              });
+              fs.writeFileSync(routesPath, JSON.stringify(routes, null, 2));
+              console.log("closeBundle routes", routes);
+              resolve();
+            }, 1000);
+          });
+        },
+      },
     ],
   };
 });
