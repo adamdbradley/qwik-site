101ba1c5e55aa1ff723ea9992a0070fe8fc1ba25
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-11-16 19:27:42 -0600

    🐳

--- src/entry.vercel-edge.tsx
+++ src/entry.vercel-edge.tsx
@@ -1,4 +1,4 @@
-import { createQwikCity } from "../fake_modules/qwik-city/middleware/vercel-edge";
+import { createQwikCity } from "@builder.io/qwik-city/middleware/vercel-edge";
 import qwikCityPlan from "@qwik-city-plan";
 import render from "./entry.ssr";
 
