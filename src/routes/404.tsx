2f897a2102a8f989b8be30d7f69089caf8978d69
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-11-16 19:22:34 -0600

    🎨

---
+++ src/routes/404.tsx
@@ -0,0 +1,14 @@
+import { component$ } from "@builder.io/qwik";
+import { DocumentHead } from "@builder.io/qwik-city";
+
+export default component$(() => {
+  return (
+    <div>
+      <h1>404!!</h1>
+    </div>
+  );
+});
+
+export const head: DocumentHead = {
+  title: "404!!",
+};
