8ebc685fedb26ad0f5e9e7ec5d295c6ea51609d8
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-12-20 12:45:18 -0600

    🍲

--- fake_modules/lib/middleware/vercel-edge/index.mjs
+++ fake_modules/lib/middleware/vercel-edge/index.mjs
@@ -1,19 +1,22 @@
 // packages/qwik-city/middleware/vercel-edge/index.ts
 import {
   mergeHeadersCookies,
-  requestHandler
+  requestHandler,
 } from "../request-handler/index.mjs";
 import { getNotFound } from "@qwik-city-not-found-paths";
 import { isStaticPath } from "@qwik-city-static-paths";
 function createQwikCity(opts) {
   async function onRequest(request) {
     try {
+      console.log("request");
       const url = new URL(request.url);
+      console.log("request url", url.href);
+      console.log("isStaticPath(url)", isStaticPath(url));
       if (isStaticPath(url)) {
         return new Response(null, {
           headers: {
-            "x-middleware-next": "1"
-          }
+            "x-middleware-next": "1",
+          },
         });
       }
       const serverRequestEv = {
@@ -25,12 +28,12 @@ function createQwikCity(opts) {
           const { readable, writable } = new TransformStream();
           const response = new Response(readable, {
             status,
-            headers: mergeHeadersCookies(headers, cookies)
+            headers: mergeHeadersCookies(headers, cookies),
           });
           resolve(response);
           return writable;
         },
-        platform: process.env
+        platform: process.env,
       };
       const handledResponse = await requestHandler(serverRequestEv, opts);
       if (handledResponse) {
@@ -39,18 +42,22 @@ function createQwikCity(opts) {
       const notFoundHtml = getNotFound(url.pathname);
       return new Response(notFoundHtml, {
         status: 404,
-        headers: { "Content-Type": "text/html; charset=utf-8", "X-Not-Found": url.pathname }
+        headers: {
+          "Content-Type": "text/html; charset=utf-8",
+          "X-Not-Found": url.pathname,
+        },
       });
     } catch (e) {
       console.error(e);
       return new Response(String(e || "Error"), {
         status: 500,
-        headers: { "Content-Type": "text/plain; charset=utf-8", "X-Error": "vercel-edge" }
+        headers: {
+          "Content-Type": "text/plain; charset=utf-8",
+          "X-Error": "vercel-edge",
+        },
       });
     }
   }
   return onRequest;
 }
-export {
-  createQwikCity
-};
+export { createQwikCity };
